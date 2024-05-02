4a986fa9984853bbd10c37e0a48b44218c0c42f6
2rang2 <rang0825@gmail.com>
2024-05-02 17:21:50 +0900

    feat: 카카오톡으로 로그인/로그아웃

--- LoginAndSignup/ViewController.swift
+++ LoginAndSignup/ViewController.swift
@@ -6,14 +6,92 @@
 //
 
 import UIKit
+import KakaoSDKUser
 
 class ViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    var userLoginState = false
+    let userLoginStateKey = "userLoginState"
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        defaults.set(userLoginState, forKey: self.userLoginStateKey)
+        userLoginState = defaults.bool(forKey: self.userLoginStateKey)
+        
+        updateUI()
+        
+        
     }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+        // 카카오톡 실행 가능 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("카카오톡으로 로그인 성공")
+                    
+                    //do something
+                    _ = oauthToken
+                    self.changeLoginState(userLoginState: true)
+                }
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                    if let error = error {
+                        print(error)
+                    }
+                    else {
+                        print("카카오 계정으로 로그인 성공")
+                        self.changeLoginState(userLoginState: true)
 
+                        //do something
+                        _ = oauthToken
 
+                    }
+                }
+        }
+        
+    }
+    
+    
+    @IBAction func logoutButtonPressed(_ sender: UIButton) {
+        UserApi.shared.logout {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("로그아웃 성공")
+                self.changeLoginState(userLoginState: false)
+            }
+        }
+    }
+    
+    func updateUI() {
+        // 로그인된 경우 로그아웃 버튼이 보이도록, 로그아웃된 경우 카카오로 로그인 버튼이 보이도록
+        if userLoginState {
+            loginButton.isHidden = true
+            logoutButton.isHidden = false
+        } else {
+            loginButton.isHidden = false
+            logoutButton.isHidden = true
+        }
+    }
+    
+    func changeLoginState(userLoginState: Bool) {
+        self.userLoginState = userLoginState
+        updateUI()
+    }
+    
 }
 
--- Podfile
+++ Podfile
@@ -1,6 +1,8 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
+source 'https://github.com/CocoaPods/Specs.git'
+
 target 'LoginAndSignup' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
@@ -8,6 +10,7 @@ target 'LoginAndSignup' do
   pod 'KakaoSDKCommon'  # 필수 요소를 담은 공통 모듈
   pod 'KakaoSDKAuth'  # 사용자 인증
   pod 'KakaoSDKUser'  # 카카오 로그인, 사용자 관리
+  pod 'GoogleSignIn'
 
   # Pods for LoginAndSignup
 
